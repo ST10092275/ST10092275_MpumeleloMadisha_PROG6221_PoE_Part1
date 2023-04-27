0c6c722d66019471b5837b7f839c9bc8c3558abc
ST10092275 <ST10092275@vcconnect.edu.za>
2023-04-27 19:10:08 +0200

    Changed quantity and unit measurement to be their own methods.

--- ST10092275_MpumeleloMadisha_PROG6221_PoE_Part1/Program.cs
+++ ST10092275_MpumeleloMadisha_PROG6221_PoE_Part1/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Drawing;
-using System.Timers;
 
 namespace recipes
 {
@@ -11,7 +10,6 @@ namespace recipes
         {
             get { return name; }
             set { name = value; }
-            
 
         }
     }
@@ -20,9 +18,8 @@ namespace recipes
 
     class Recipe
     {
-        static void NoIngredients()
+        static void  NoIngredients()
         {
-            
 
             int size = Convert.ToInt32(Console.ReadLine());
             string[] array = new string[size];
@@ -31,20 +28,29 @@ namespace recipes
             {
                 Console.WriteLine("Please enter the name of the ingredient");
                 array[a] = Console.ReadLine();
-                Console.Clear();
             }
+        }
+        static void Quantity()
+        {
+
+            int size = int.Parse(Console.ReadLine());
+            string[] array = new string[size];
             for (int b = 0; b < size; b++)
             {
                 Console.WriteLine("Please enter the quantity of ingredient");
                 array[b] = Console.ReadLine();
-                Console.Clear();
             }
+        }
+        static void Measurement() {
+
+            int size = Convert.ToInt32(Console.ReadLine());
+            string[] array = new string[size];
             for (int c = 0; c < size; c++)
             {
                 Console.WriteLine("Please enter the measurement unit of ingredient");
                 array[c] = Console.ReadLine();
-                Console.Clear();
             }
+            
         }
 
 
@@ -54,18 +60,19 @@ namespace recipes
 
             int number = Convert.ToInt32(Console.ReadLine());
             string[] arr = new string[number];
-            Console.Clear();
+
             for (int i = 0; i < number; i++)
             {
                 Console.WriteLine("Please enter the name of the ingredient");
                 arr[i] = Console.ReadLine();
-                Console.Clear();
+
             }
         }
         public static void Main(string[] args)
         {
             Console.Title = "PROG6221_PART1";
             Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.BackgroundColor = ConsoleColor.Black;
             Console.WriteLine("********************************************************************");
             Console.WriteLine("                Welcome to Sanele's recipe app  ");
             Console.WriteLine("               Please enter an option to proceed");
@@ -75,23 +82,28 @@ namespace recipes
             Console.WriteLine("1) Add Recipe");
             Console.WriteLine("2) Exit Application");
             int firststep = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-
-            Console.WriteLine("Please enter an option to proceed to entering your recipe");
-
+           
             if (firststep == 1)
             {
-
+                Console.WriteLine("Please press any key to proceed");
                 RecipeName myObj = new RecipeName();
                 Console.WriteLine("What is the name of the recipe?");
                 myObj.Name = Convert.ToString(Console.ReadLine());
-                Console.Clear();
-
+                Console.Clear() ;
                 Console.WriteLine("How many ingredients would you like to enter for the " + myObj.Name + " recipe?");
                 Console.Clear();
 
                 NoIngredients();
+                Console.WriteLine("Please enter any key to proceed");
+                Console.Clear();
+                Measurement();
+                Console.WriteLine("Please enter any key to proceed");
+                Console.Clear();
+                Quantity();
+                Console.WriteLine("Please enter any key to proceed");
+                Console.Clear();
                 NoSteps();
+                Console.Clear();
 
                 Console.WriteLine("Select an option");
                 Console.WriteLine("1) Main Menu.");
@@ -101,7 +113,20 @@ namespace recipes
                 Console.WriteLine("********************************************************************");
                 Console.WriteLine("                          Main Menu");
                 Console.WriteLine("********************************************************************");
+                Console.WriteLine("1) Print Recipe");
+                Console.WriteLine("2) Exit Application.");
                 Console.ResetColor();
+
+                int step = Convert.ToInt32(Console.ReadLine());
+
+                if (step == 1)
+                {
+                    
+                    Console.WriteLine("Here is the recipe for " + myObj);
+                    Console.WriteLine("********************************************************************");
+                    
+                }
+                
             }
             if (firststep == 2)
             {
